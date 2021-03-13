0dcbd78fc50849b0a38e112659a9ce0823adb452
Mustafa H <mhaid@cs.toronto.edu>
2021-03-13 15:01:49 -0500

    Upload media folder

--- src/graphs/model/GCN.py
+++ src/graphs/model/GCN.py
@@ -8,12 +8,11 @@ class SimpleGCNModel(nn.Module):
 
     def __init__(self, num_node_features, num_output_channels):
         super(SimpleGCNModel, self).__init__()
-        intermediate_1 = num_node_features * 2
-        intermediate_2 = num_node_features * 4
-        self.conv1 = GCNConv(num_node_features, intermediate_1)
-        self.conv2 = GCNConv(intermediate_1, intermediate_2)
-        self.conv3 = GCNConv(intermediate_2, intermediate_2)
-        self.lin = nn.Linear(intermediate_2, num_output_channels)
+        hidden_layers = 64
+        self.conv1 = GCNConv(num_node_features, hidden_layers)
+        self.conv2 = GCNConv(hidden_layers, hidden_layers)
+        self.conv3 = GCNConv(hidden_layers, hidden_layers)
+        self.lin = nn.Linear(hidden_layers, num_output_channels)
 
     def forward(self, x, edge_index, batch):
 
